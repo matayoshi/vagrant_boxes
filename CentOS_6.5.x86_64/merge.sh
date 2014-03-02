57ee6ee2e12c92eb2f8835b81ba2d2c4f3f2618e
N.Matayoshi <n.matayoshi@gmail.com>
2014-03-02 20:49:17 +0900

    fix merge script

--- CentOS_6.5.x86_64/merge.sh
+++ CentOS_6.5.x86_64/merge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 input_file_prefix="centos6.5.x86_64.20140301.box."
-output_file="centos6.5.x86_64.20140301.box.test"
+output_file="centos6.5.x86_64.20140301.box"
 
 cat "${input_file_prefix}"[a-z][a-z] > "${output_file}"
 
